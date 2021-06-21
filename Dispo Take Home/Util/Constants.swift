267a3c1183511008f704bc8d5e940479d895f41c
Malone Hedges <malonehedges@users.noreply.github.com>
2021-06-21 15:06:09 -0700

    Remove api key

--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -2,5 +2,5 @@ enum Constants {}
 
 extension Constants {
   // Get an API key from https://developers.giphy.com/dashboard/
-  static let giphyApiKey = "0xHnIT9HqpBIh0XYygDKo4iVXqB8qo0G"
+  static let giphyApiKey = ""
 }
